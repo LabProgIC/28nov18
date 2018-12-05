67f47a13da1824238d3a5c2fffa5161ba5ed4812
itacirgabral <itacirgabral@users.noreply.github.com>
2018-12-05 17:06:23 -0300

    remove unused files

--- compra/testTabela.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "tabelaPreco.c"
-
-void main () {
-	printf("para 5 temos %d\n", tabelaPreco(5));
-	printf("para 15 temos %d\n", tabelaPreco(15));
-	printf("para 400 temos %d\n", tabelaPreco(400));
-	printf("para -100 temos %d\n", tabelaPreco(-100));
-}
